2abe40c9031c4a7f559cd730affbe0dd19d78439
Leekangmin <kkk5474096@naver.com>
2022-10-06 18:35:06 +0900

    [#4] titleSection & buttonSection

--- lib/base_layout/layout.dart
+++ lib/base_layout/layout.dart
@@ -7,39 +7,51 @@ void main() {
 class LayoutApp extends StatelessWidget {
   LayoutApp({super.key});
 
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(color: Colors.grey[500]),
-                )
-              ],
-            )
-        ),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        const Text('41')
-      ],
-    ),
-  );
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE')
+      ],
+    );
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'Oeschinen Lake Campground',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    'Kandersteg, Switzerland',
+                    style: TextStyle(color: Colors.grey[500]),
+                  )
+                ],
+              )
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41')
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: "Flutter layout demo",
       home: Scaffold(
@@ -47,9 +59,35 @@ class LayoutApp extends StatelessWidget {
           title: const Text("Flutter layout demo"),
         ),
         body: Column(
-          children: [titleSection],
+          children: [
+            titleSection,
+            buttonSection
+          ],
         ),
       ),
     );
   }
+}
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+          icon, color: color
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color
+          ),
+        ),
+      )
+    ],
+  );
 }
