11fe217486531cac81472cc7bc6f2032b8b26478
Leekangmin <kkk5474096@naver.com>
2022-10-12 18:48:28 +0900

    로고 커지는 애니메이션 적용

--- lib/animation/animation.dart
+++ lib/animation/animation.dart
@@ -14,17 +14,39 @@ class AnimationApp extends StatefulWidget {
   State<AnimationApp> createState() => _AnimationAppState();
 }
 
-class _AnimationAppState extends State<AnimationApp> {
+class _AnimationAppState extends State<AnimationApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    ..addListener(() {
+      setState(() {
+
+      });
+    });
+    controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
+        height: animation.value,
+        width: animation.value,
         child: const FlutterLogo(),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
