390ffd59e827cb517bd1502b154cc092a0be3b90
Leekangmin <kkk5474096@naver.com>
2022-10-13 14:18:12 +0900

    [#8] Simplifying with Animated­Widget

--- lib/animation/animation.dart
+++ lib/animation/animation.dart
@@ -6,6 +6,26 @@ void main() {
   runApp(const AnimationApp());
 }
 
+// 2번째
+class AnimationLogo extends AnimatedWidget {
+  const AnimationLogo({super.key, required Animation<double> animation})
+  : super(listenable: animation);
+
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width:  animation.value,
+        child: const FlutterLogo()
+      ),
+    );
+  }
+}
+
 class AnimationApp extends StatefulWidget {
   const AnimationApp({super.key});
 
@@ -18,30 +38,34 @@ class _AnimationAppState extends State<AnimationApp> with SingleTickerProviderSt
   late Animation<double> animation;
   late AnimationController controller;
 
-
   @override
   void initState() {
     super.initState();
     controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-    ..addListener(() {
-      setState(() {
-
-      });
-    });
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    // 1번째
+    // animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    // ..addListener(() {
+    //   setState(() {
+    //
+    //   });
+    // });
     controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
-      ),
-    );
+    return AnimationLogo(animation: animation);
+    // 1번째
+    // return Center(
+    //   child: Container(
+    //     margin: const EdgeInsets.symmetric(vertical: 10),
+    //     height: animation.value,
+    //     width: animation.value,
+    //     child: const FlutterLogo(),
+    //   ),
+    // );
+    return AnimationLogo(animation: animation);
   }
 
   @override
